ab29ff7fd89e1c59d1a599c426cd9524625b0d22
Phucndh16 <phucndh1610@gmail.com>
2023-02-03 08:41:59 +0700

    config and setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+// import reactlogo from './assets/images/react.svg'
+// import './assets/styles/app.css'
+// import './assets/styles/app.scss'
+
+const App: FC = () => {
+  return <div>build webpack</div>
+}
+
+export default App
