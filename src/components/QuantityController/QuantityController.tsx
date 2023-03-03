e51d7b4c89faf1b2d3b3efa5b7dfa8561ce4fdae
Phucndh1610 <87517780+Phucndh1610@users.noreply.github.com>
2023-03-04 01:43:20 +0700

    create ui cart and handle logic add cart (#20)

--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -1,10 +1,12 @@
 import InputNumber, { InputNumberProps } from '@Components/InputNumber/index'
+import { useState } from 'react'
 
 interface Props extends InputNumberProps {
   max?: number
   onIncrease?: (value: number) => void
   onDecrease?: (value: number) => void
   onType?: (value: number) => void
+  onFocusOut?: (value: number) => void
   classNameWrapper?: string
 }
 
@@ -13,10 +15,12 @@ export default function QuantityController({
   onIncrease,
   onDecrease,
   onType,
+  onFocusOut,
   value,
   classNameWrapper = 'ml-10',
   ...rest
 }: Props) {
+  const [localValue, setLocalValue] = useState<number>(Number(value || 0))
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(event.target.value)
     if (max !== undefined && _value > max) {
@@ -25,22 +29,29 @@ export default function QuantityController({
       _value = 1
     }
     onType && onType(_value)
+    setLocalValue(_value)
   }
 
   const increase = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && value === max) {
       _value = max
     }
     onIncrease && onIncrease(_value)
+    setLocalValue(_value)
   }
 
   const decrease = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
     onDecrease && onDecrease(_value)
+    setLocalValue(_value)
+  }
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
+    onFocusOut && onFocusOut(Number(event.target.value))
   }
 
   return (
@@ -61,10 +72,11 @@ export default function QuantityController({
         </svg>
       </button>
       <InputNumber
-        value={value}
+        value={value || localValue}
         className=''
         classNameError='hidden'
         classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
+        onBlur={handleBlur}
         onChange={handleChange}
         {...rest}
       />
@@ -78,7 +90,7 @@ export default function QuantityController({
           viewBox='0 0 24 24'
           strokeWidth={1.5}
           stroke='currentColor'
-          className='h-6 w-6'
+          className='h-4 w-4'
         >
           <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
         </svg>
---
+++ src/layouts/CartLayout/CartLayout.tsx
@@ -0,0 +1,16 @@
+import CartHeader from '@Components/CartHeader'
+import Footer from '@Components/Footer'
+
+interface Props {
+  children?: React.ReactNode
+}
+
+export default function CartLayout({ children }: Props) {
+  return (
+    <div>
+      <CartHeader />
+      {children}
+      <Footer />
+    </div>
+  )
+}
