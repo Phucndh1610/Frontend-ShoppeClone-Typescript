d1b9b35b6ab7f2c28d367105778fb31a5a408c35
Phucndh1610 <87517780+Phucndh1610@users.noreply.github.com>
2023-03-08 02:08:20 +0700

    handle refresh token (#31)

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,15 +3,21 @@ import { User } from '@Types/user.type'
 export const setAccessTokenToLS = (access_token: string) => {
   localStorage.setItem('access_token', access_token)
 }
+export const setRefreshTokenToLS = (refresh_token: string) => {
+  localStorage.setItem('refresh_token', refresh_token)
+}
 
 export const clearLS = () => {
   localStorage.removeItem('access_token')
+  localStorage.removeItem('refresh_token')
   localStorage.removeItem('profile')
   const clearLSEvent = new Event('clearLS')
   LocalStorageEventTarget.dispatchEvent(clearLSEvent)
 }
 
 export const getAccessTokenFromLS = () => localStorage.getItem('access_token') || ''
+
+export const getRefreshTokenFromLS = () => localStorage.getItem('refresh_token') || ''
 
 export const getProfile = () => {
   const result = localStorage.getItem('profile')
