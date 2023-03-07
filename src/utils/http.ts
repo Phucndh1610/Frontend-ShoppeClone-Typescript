37091b706434fdda1f167f70241f75283c19ad05
Phucndh16 <phucndh1610@gmail.com>
2023-03-08 02:07:33 +0700

    handle refresh token

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,21 +1,36 @@
 import axios, { AxiosError, type AxiosInstance } from 'axios'
 import HttpStatusCode from 'src/constants/httpStatusCode.enum'
 import { toast } from 'react-toastify'
-import { AuthResponse } from '@Types/auth.type'
-import { clearLS, getAccessTokenFromLS, setAccessTokenToLS, setProfile } from './auth'
-import { URL_LOGIN, URL_LOGOUT, URL_REGISTER } from '@Apis/auth.api'
+import { AuthResponse, RefreshTokenResponse } from '@Types/auth.type'
+import {
+  clearLS,
+  getAccessTokenFromLS,
+  getRefreshTokenFromLS,
+  setAccessTokenToLS,
+  setProfile,
+  setRefreshTokenToLS
+} from './auth'
+import { URL_LOGIN, URL_LOGOUT, URL_REGISTER, URL_REFRESH_TOKEN } from '@Apis/auth.api'
 import config from 'src/constants/config'
+import { isAxiosExpiredTokenError, isAxiosUnauthorizedError } from './utils'
+import { ErrorResponse } from '@Types/utils.type'
 
-class Http {
+export class Http {
   instance: AxiosInstance
   private accessToken: string
+  private refreshToken: string
+  private refreshTokenRequest: Promise<string> | null
   constructor() {
     this.accessToken = getAccessTokenFromLS()
+    this.refreshToken = getRefreshTokenFromLS()
+    this.refreshTokenRequest = null
     this.instance = axios.create({
       baseURL: config.baseUrl,
       timeout: 10000,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'expire-access-token': 5,
+        'expire-refresh-token': 60 * 60
       }
     })
     this.instance.interceptors.request.use(
@@ -37,26 +52,70 @@ class Http {
         if (url === URL_LOGIN || url === URL_REGISTER) {
           const data = response.data as AuthResponse
           this.accessToken = data.data.access_token
+          this.refreshToken = data.data.refresh_token
           setAccessTokenToLS(this.accessToken)
+          setRefreshTokenToLS(this.refreshToken)
           setProfile(data.data.user)
         } else if (url === URL_LOGOUT) {
           this.accessToken = ''
+          this.refreshToken = ''
           clearLS()
         }
         return response
       },
       (error: AxiosError) => {
-        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+        if (
+          ![HttpStatusCode.UnprocessableEntity, HttpStatusCode.Unauthorized].includes(error.response?.status as number)
+        ) {
           const data: any | undefined = error.response?.data
           const message = data?.message || error.message
           toast.error(message)
         }
-        if (error.response?.status === HttpStatusCode.Unauthorized) {
+        // error 401
+        if (isAxiosUnauthorizedError<ErrorResponse<{ name: string; message: string }>>(error)) {
+          // case expired token and request !== request refresh token
+          // Conduct calling refresh token
+          if (isAxiosExpiredTokenError(error) && error.config?.url !== URL_REFRESH_TOKEN) {
+            this.refreshTokenRequest = this.refreshTokenRequest
+              ? this.refreshTokenRequest
+              : this.handleRefreshToken().finally(() => {
+                  setTimeout(() => {
+                    this.refreshTokenRequest = null
+                  }, 10000)
+                })
+            return this.refreshTokenRequest.then((access_token) => {
+              return this.instance({
+                ...(error.response?.config || {}),
+                headers: { ...(error.response?.config.headers || {}), Authorization: access_token }
+              })
+            })
+          }
           clearLS()
+          this.accessToken = ''
+          this.refreshToken = ''
+          toast.error(error.response?.data.data?.message)
         }
         return Promise.reject(error)
       }
     )
+  }
+  private handleRefreshToken() {
+    return this.instance
+      .post<RefreshTokenResponse>(URL_REFRESH_TOKEN, {
+        refresh_token: this.refreshToken
+      })
+      .then((res) => {
+        const { access_token } = res.data.data
+        setAccessTokenToLS(access_token)
+        this.accessToken = access_token
+        return access_token
+      })
+      .catch((error) => {
+        clearLS()
+        this.accessToken = ''
+        this.refreshToken = ''
+        throw error
+      })
   }
 }
 const http = new Http().instance
